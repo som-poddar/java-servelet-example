4230c4d68681fa9d4601a68686f372da7c849bdc
Som Poddar <som.poddar@warnermedia.com>
2023-05-21 23:06:19 -0700

    Working

---
+++ app/src/main/java/app/ApiServlet.java
@@ -0,0 +1,99 @@
+package app;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.GZIPOutputStream;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.Histogram;
+import io.prometheus.client.Summary;
+import io.prometheus.client.exporter.common.TextFormat;
+
+public class ApiServlet extends HttpServlet {
+    private static final String NAMESPACE_JAVA_APP="servlet_example";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+                    CollectorRegistry registry = CollectorRegistry.defaultRegistry;
+                    populateMetric(registry);
+                    response.getWriter().println("{ \"status\": \"ok (populateMetric)\"}");
+                    response.setStatus(HttpServletResponse.SC_OK);
+
+                /*
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                    response.getWriter().println("{ \"status\": \"not ok \"}");
+                    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                }*/
+        response.setContentType("application/json");
+    }
+
+        private static void populateMetric(CollectorRegistry registry) {
+        Counter counter = counter(registry);
+
+        Gauge gauge = guage(registry);
+        Histogram histogram = histogram();
+        Summary summary = summary(registry);
+                    counter.inc(rand(0, 5));
+                    // gauge.set(rand(-5, 10));
+                    // histogram.observe(rand(0, 5));
+                    // summary.observe(rand(0, 5));
+                }
+private static Summary summary(CollectorRegistry registry) {
+        return Summary.build()
+                .namespace(NAMESPACE_JAVA_APP)
+                .name("s")
+                .help("s summary")
+                .register(registry);
+    }
+
+    private static Histogram histogram() {
+        return Histogram.build()
+                .namespace(NAMESPACE_JAVA_APP)
+                .name("h")
+                .help("h help")
+                .register();
+    }
+
+    private static Gauge guage(CollectorRegistry registry) {
+        return Gauge.build()
+                .namespace("java")
+                .name("g")
+                .help("g healp")
+                .register(registry);
+    }
+
+    private static Counter counter(CollectorRegistry registry) {
+        return Counter.build()
+                .namespace(NAMESPACE_JAVA_APP)
+                .name("a")
+                .help("a help")
+                .register(registry);
+    }
+
+    private static double rand(double min, double max) {
+        return min + (Math.random() * (max - min));
+    }
+    }
